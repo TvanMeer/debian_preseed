e345d1a1141458ec03e4ed6d7217d7b65c34933d
TvanMeer <55160903+TvanMeer@users.noreply.github.com>
2022-05-02 06:00:40 +0200

    Fix relative paths

--- create_iso.sh
+++ create_iso.sh
@@ -9,6 +9,8 @@ cd $(dirname $0)
 curl -O https://laotzu.ftp.acc.umu.se/debian-cd/current/amd64/iso-cd/debian-11.3.0-amd64-netinst.iso
 
 
+# See https://wiki.debian.org/DebianInstaller/Preseed/EditIso
+
 # Extracting the Initrd from an ISO Image
 sudo apt install libarchive-tools
 mkdir iso
@@ -31,23 +33,31 @@ chmod -w md5sum.txt
 cd ..
 
 
-# Repack as EFI based bootable ISO, see https://wiki.debian.org/RepackBootableISO
-orig_iso=debian-11.3.0-amd64-netinst.iso
-new_files=iso_unpacked_and_modified
-new_iso=debian-11.3.0-amd64-modified.iso
-mbr_template=isohdpfx.bin
+# See https://wiki.debian.org/RepackBootableISO
+
+# Repack as EFI based bootable ISO
+current_path=$(pwd)
+
+orig_iso=$current_path/debian-11.3.0-amd64-netinst.iso
+new_files=$current_path/iso
+new_iso=$current_path/debian-11.3.0-amd64-modified.iso
+mbr_template=$current_path/isohdpfx.bin
 
 
 # Extract MBR template file to disk
 dd if="$orig_iso" bs=1 count=432 of="$mbr_template"
 
 
-# Modified contents of file iso/.disk/mkisofs
+# Install xorriso packager
+sudo apt-get -f install xorriso
+
+
+# Run modified contents of file iso/.disk/mkisofs
 xorriso -as mkisofs \
    -V 'Debian 11.3.0 amd64 n' \
    -o debian-11.3.0-amd64-modified.iso \
    -J -joliet-long -cache-inodes \
-   -isohybrid-mbr syslinux/usr/lib/ISOLINUX/isohdpfx.bin \
+   -isohybrid-mbr "$mbr_template" \
    -b isolinux/isolinux.bin \
    -c isolinux/boot.cat \
    -boot-load-size 4 -boot-info-table -no-emul-boot \
@@ -56,4 +66,4 @@ xorriso -as mkisofs \
    -no-emul-boot \
    -isohybrid-gpt-basdat \
    -isohybrid-apm-hfsplus \
-   boot1 CD1
+   "$new_files"
